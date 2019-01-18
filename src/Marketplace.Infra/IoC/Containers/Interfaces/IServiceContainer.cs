8ee0e31915a2d774aa9005502ff7ffb4b1cc03c9
Davi Coutinho <davi.roox@hotmail.com>
2019-01-18 14:38:40 -0200

    interface de container removida - utilizar futuramente com o aumento de services e DI

--- src/Marketplace.Infra/IoC/Containers/Interfaces/IServiceContainer.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace Marketplace.Infra.IoC.Containers.Interfaces
-{
-    public interface IServiceContainer
-    {
-        void AddServices(IServiceCollection services);
-    }
-}
