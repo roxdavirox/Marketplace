5afb6c994b61971295f62e022586ab624ded3337
Davi Coutinho <davi.roox@hotmail.com>
2019-01-21 17:12:00 -0200

    usings desnecessarios removidos

--- src/Marketplace.Domain/Entities/Product.cs
+++ src/Marketplace.Domain/Entities/Product.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Marketplace.Domain.Entities.Base;
+﻿using Marketplace.Domain.Entities.Base;
 using Marketplace.Domain.Validators;
 
 namespace Marketplace.Domain.Entities
