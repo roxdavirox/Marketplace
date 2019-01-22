0b888028e653367d6e56904cc514c5ef2e40f50f
Davi Coutinho <davi.roox@hotmail.com>
2019-01-22 17:40:38 -0200

    post item controller

---
+++ src/Marketplace.Api/Controllers/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using Marketplace.App.Services.Handlers.Items;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Marketplace.Api.Controllers
+{
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ItemController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("api/Items")]
+        public async Task<CreateItemResponse> Post(CreateItemRequest request) =>
+            await _mediator.Send(request);
+    }
+}
