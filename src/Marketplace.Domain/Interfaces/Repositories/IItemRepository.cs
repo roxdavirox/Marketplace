b34f4e8378f99abbd63b7e818eb7141ef6b29c8d
Davi Coutinho <davi.roox@hotmail.com>
2019-01-22 16:53:16 -0200

    interface de depositorio de item adicionado

---
+++ src/Marketplace.Domain/Interfaces/Repositories/IItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using Marketplace.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Marketplace.Domain.Interfaces.Repositories
+{
+    public interface IItemRepository
+    {
+        Task<int> CreateAsync(Item item);
+    }
+}
