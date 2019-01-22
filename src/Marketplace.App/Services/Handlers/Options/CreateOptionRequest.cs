d05682159c22f037e5e557e484682036e563c996
Davi Coutinho <davi.roox@hotmail.com>
2019-01-22 14:35:03 -0200

    propriedade idProduct removido, relação entre entidades com verbo post encapsulado em arquivo separado

--- src/Marketplace.App/Services/Handlers/Options/CreateOptionRequest.cs
+++ src/Marketplace.App/Services/Handlers/Options/CreateOptionRequest.cs
@@ -1,11 +1,9 @@
 ﻿using MediatR;
-using System;
 
 namespace Marketplace.App.Services.Handlers.Options
 {
-    public class CreateOptionRequest : IRequest<CreateOptionResponse>
+    public class CreateOptionRequest : IRequest<CreateOptionResponse> 
     {
-        public Guid IdProduct { get; set; }
         public string Name { get; set; }
     }
 }
