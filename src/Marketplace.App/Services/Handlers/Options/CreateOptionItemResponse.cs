609792b254ce6b911f8d5eb7d931c57ced885682
Davi Coutinho <davi.roox@hotmail.com>
2019-01-23 18:09:05 -0200

    associação de Opção com item existente - handler, request e response (#15)

---
+++ src/Marketplace.App/Services/Handlers/Options/CreateOptionItemResponse.cs
@@ -0,0 +1,22 @@
+﻿using Marketplace.Domain.Entities;
+using System;
+
+namespace Marketplace.App.Services.Handlers.Options
+{
+    public class CreateOptionItemResponse
+    {
+        public string ItemName { get; set; }
+        public Guid IdItem { get; set; }
+        public string OptionName { get; set; }
+        public Guid IdOption { get; set; }
+
+        public CreateOptionItemResponse(Item item, Option option)
+        {
+            ItemName = item.Name;
+            IdItem = item.Id;
+
+            OptionName = option.Name;
+            IdOption = option.Id;
+        }
+    }
+}
