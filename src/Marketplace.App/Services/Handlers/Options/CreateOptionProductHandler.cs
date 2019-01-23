504b3e04d913460173160b12f5c09982d2d4739e
Davi Coutinho <davi.roox@hotmail.com>
2019-01-23 09:10:11 -0200

    option product refatorado

---
+++ src/Marketplace.App/Services/Handlers/Options/CreateOptionProductHandler.cs
@@ -0,0 +1,50 @@
+﻿using Marketplace.App.Notifications;
+using Marketplace.Domain.Entities;
+using Marketplace.Domain.Interfaces.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Marketplace.App.Services.Handlers.Options
+{
+    public class CreateOptionProductHandler : IRequestHandler<CreateOptionProductRequest, CreateOptionResponse>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IOptionRepository _optionRepository;
+        private readonly NotificationContext _notificationContext;
+        
+        public CreateOptionProductHandler(
+            IProductRepository productRepository, 
+            IOptionRepository optionRepository, 
+            NotificationContext notificationContext)
+        {
+            _productRepository = productRepository;
+            _optionRepository = optionRepository;
+            _notificationContext = notificationContext;
+        }
+
+        public async Task<CreateOptionResponse> Handle(
+            CreateOptionProductRequest request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.IdProduct);
+
+            if(product == null)
+            {
+                _notificationContext.AddNotification("product null", "O produto não foi encontrado");
+                return null;
+            }
+
+            var option = new Option(request.Name, product);
+
+            if(option.Invalid)
+            {
+                _notificationContext.AddNotifications(option.ValidationResult);
+                return null;
+            }
+
+            await _optionRepository.CreateAsync(option);
+
+            return (CreateOptionResponse)option;
+        }
+    }
+}
---
+++ src/Marketplace.App/Services/Handlers/Options/CreateOptionProductRequest.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+
+namespace Marketplace.App.Services.Handlers.Options
+{
+    public class CreateOptionProductRequest : IRequest<CreateOptionResponse>
+    {
+        internal Guid IdProduct { get; private set; }
+        public string Name { get; set; }
+
+        public CreateOptionProductRequest()
+        {
+
+        }
+
+        public CreateOptionProductRequest(CreateOptionProductRequest request, Guid idProduct)
+        {
+            Name = request.Name;
+            IdProduct = idProduct;
+        }
+    }
+}
