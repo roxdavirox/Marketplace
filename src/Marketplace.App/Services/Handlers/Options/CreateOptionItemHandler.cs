82eccecad98fd4f32bd42bc507a104c0b871e687
Davi Coutinho <davi.roox@hotmail.com>
2019-01-23 18:09:05 -0200

    associação de Opção com item existente - handler, request e response (#15)

---
+++ src/Marketplace.App/Services/Handlers/Options/CreateOptionItemHandler.cs
@@ -0,0 +1,52 @@
+﻿using Marketplace.App.Notifications;
+using Marketplace.Domain.Entities;
+using Marketplace.Domain.Interfaces.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Marketplace.App.Services.Handlers.Options
+{
+    public class CreateOptionItemHandler : IRequestHandler<CreateOptionItemRequest, CreateOptionItemResponse>
+    {
+        private readonly NotificationContext _notificationContext;
+        private readonly IItemRepository _itemRepository;
+        private readonly IOptionRepository _optionRepository;
+
+        public CreateOptionItemHandler(
+            NotificationContext notificationContext, 
+            IItemRepository itemRepository, 
+            IOptionRepository optionRepository)
+        {
+            _notificationContext = notificationContext;
+            _itemRepository = itemRepository;
+            _optionRepository = optionRepository;
+        }
+
+        public async Task<CreateOptionItemResponse> Handle(
+            CreateOptionItemRequest request, CancellationToken cancellationToken)
+        {
+            var item = await _itemRepository.GetByIdAsync(request.IdItem);
+
+            if(item == null)
+            {
+                _notificationContext.AddNotification("Item null", "Item não encontrado");
+                return null;
+            }
+
+            var option = new Option(request.Name);
+
+            if (option.Invalid)
+            {
+                _notificationContext.AddNotifications(option.ValidationResult);
+                return null;
+            }
+
+            item.AssociateWith(option);
+
+            await _optionRepository.CreateAsync(option);
+
+            return new CreateOptionItemResponse(item, option);
+        }
+    }
+}
---
+++ src/Marketplace.App/Services/Handlers/Options/CreateOptionItemRequest.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marketplace.App.Services.Handlers.Options
+{
+    public class CreateOptionItemRequest : IRequest<CreateOptionItemResponse>
+    {
+        public string Name { get; set; }
+        internal Guid IdItem { get; set; }
+
+        public CreateOptionItemRequest(CreateOptionItemRequest request, Guid idItem)
+        {
+            Name = request.Name;
+            IdItem = idItem;
+        }
+    }
+}
