4de37337cde35d8c5f4aef63b188f6c4c4f24ce4
Davi Coutinho <davi.roox@hotmail.com>
2019-01-23 09:13:28 -0200

    opcao do item request

---
+++ src/Marketplace.App/Services/Handlers/Items/CreateItemOptionRequest.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+
+namespace Marketplace.App.Services.Handlers.Items
+{
+    public class CreateItemOptionRequest : IRequest<CreateItemResponse>
+    {
+        public CreateItemOptionRequest(CreateItemOptionRequest request, Guid idOption)
+        {
+            IdOption = idOption;
+            Name = request.Name;
+        }
+
+        public CreateItemOptionRequest() { }
+
+        internal Guid IdOption { get; set; }
+        public string Name { get; set; }
+    }
+}
