5fb8340e14229c1721d87d6876abd6ddf50abdf1
Davi Coutinho <davi.roox@hotmail.com>
2019-01-23 17:15:31 -0200

    put hander, request e response - associação entre produto existente e opção existente implementado

---
+++ src/Marketplace.App/Services/Handlers/Products/PutProductOptionRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+
+namespace Marketplace.App.Services.Handlers.Products
+{
+    public class PutProductOptionRequest : IRequest<PutProductOptionResponse>
+    {
+        public Guid IdProduct { get; set; }
+        public Guid IdOption { get; set; }
+    }
+}
