7282b2386025c3ac541f46738ada9657cc52e12d
Davi Coutinho <davi.roox@hotmail.com>
2019-01-23 17:15:31 -0200

    put hander, request e response - associação entre produto existente e opção existente implementado

---
+++ src/Marketplace.App/Services/Handlers/Products/PutProductOptionHandler.cs
@@ -0,0 +1,49 @@
+﻿using Marketplace.App.Notifications;
+using Marketplace.Domain.Interfaces.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Marketplace.App.Services.Handlers.Products
+{
+    public class PutProductOptionHandler : IRequestHandler<PutProductOptionRequest, PutProductOptionResponse>
+    {
+        private readonly NotificationContext _notificationContext;
+        private readonly IProductRepository _productRepository;
+        private readonly IOptionRepository _optionRepository;
+
+        public PutProductOptionHandler(
+            NotificationContext notificationContext, 
+            IProductRepository productRepository, 
+            IOptionRepository optionRepository)
+        {
+            _notificationContext = notificationContext;
+            _productRepository = productRepository;
+            _optionRepository = optionRepository;
+        }
+
+        public async Task<PutProductOptionResponse> Handle(
+            PutProductOptionRequest request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.IdProduct);
+
+            if(product == null)
+            {
+                _notificationContext.AddNotification("Product null", "Produto não encontrado");
+                return null;
+            }
+
+            var option = await _optionRepository.GetByIdAsync(request.IdOption);
+
+            if(option == null)
+            {
+                _notificationContext.AddNotification("Option null", "Opção não encontrada");
+                return null;
+            }
+
+            option.AssociateWith(product);
+
+            return new PutProductOptionResponse(product, option);
+        }
+    }
+}
---
+++ src/Marketplace.App/Services/Handlers/Products/PutProductOptionResponse.cs
@@ -0,0 +1,23 @@
+﻿using Marketplace.Domain.Entities;
+using System;
+
+namespace Marketplace.App.Services.Handlers.Products
+{
+    public class PutProductOptionResponse
+    {
+        public string ProductName { get; set; }
+        public Guid IdProduct { get; set; }
+
+        public string OptionName { get; set; }
+        public Guid IdOption { get; set; }
+
+        public PutProductOptionResponse(Product product, Option option)
+        {
+            ProductName = product.Name;
+            IdProduct = product.Id;
+
+            OptionName = option.Name;
+            IdOption = option.Id;
+        }
+    }
+}
