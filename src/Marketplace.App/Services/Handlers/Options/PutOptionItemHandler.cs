5b8b249935f117811f7b5747b0b7466035a6e0d2
Davi Coutinho <davi.roox@hotmail.com>
2019-01-23 11:54:51 -0200

    using desnecessario removido

--- src/Marketplace.App/Services/Handlers/Options/PutOptionItemHandler.cs
+++ src/Marketplace.App/Services/Handlers/Options/PutOptionItemHandler.cs
@@ -1,9 +1,6 @@
 ﻿using Marketplace.App.Notifications;
 using Marketplace.Domain.Interfaces.Repositories;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
