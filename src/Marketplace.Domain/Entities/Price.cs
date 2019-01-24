c6c4539ecab193d0ebfdca091c2e9b12b78ec5c4
Davi Coutinho <davi.roox@hotmail.com>
2019-01-24 13:39:23 -0200

    Preco com propriedade fixa blindada

---
+++ src/Marketplace.Domain/Entities/Price.cs
@@ -0,0 +1,14 @@
+﻿using Marketplace.Domain.Entities.Base;
+
+namespace Marketplace.Domain.Entities
+{
+    public class Price : EntityBase
+    {
+        public Price(bool @fixed)
+        {
+            Fixed = @fixed;
+        }
+
+        public bool Fixed { get; private set; }
+    }
+}
