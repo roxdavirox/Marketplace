35fe36e37d2fc67dd4c2644e2b0a40c53da55485
Davi Coutinho <davi.roox@hotmail.com>
2019-01-24 16:31:51 -0200

    regra de negocio para criação de preço fixo e relativo atribuido dentro do service handler do create item request (#17)

--- src/Marketplace.App/Services/Handlers/Items/CreateItemHandler.cs
+++ src/Marketplace.App/Services/Handlers/Items/CreateItemHandler.cs
@@ -2,6 +2,8 @@
 using Marketplace.Domain.Entities;
 using Marketplace.Domain.Interfaces.Repositories;
 using MediatR;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -21,6 +23,8 @@ namespace Marketplace.App.Services.Handlers.Items
         public async Task<CreateItemResponse> Handle(
             CreateItemRequest request, CancellationToken cancellationToken)
         {
+            var price = request.GetPrice();
+
             var item = new Item(request.Name);
 
             if(item.Invalid)
@@ -33,5 +37,26 @@ namespace Marketplace.App.Services.Handlers.Items
 
             return (CreateItemResponse)item;
         }
+    }
+
+    internal static class CreateItemRequestExtension
+    {
+        public static Price GetPrice(this CreateItemRequest request) =>
+            request.Fixed 
+                ? CreateFixedPrice(request) 
+                : CreateRelativePrice(request);
+
+        private static Price CreateFixedPrice(CreateItemRequest request) =>
+            new Price(request.Fixed, request.FixedPrice.Value);
+
+        private static IEnumerable<PriceInterval> GetPriceIntervals(this CreateItemRequest request) =>
+            request.RelativePrice.Table
+                .Select(i => new PriceInterval(i.Start, i.End, i.Value));
+
+        private static RelativePrice GetRelativePrice(this CreateItemRequest request) =>
+            new RelativePrice(request.GetPriceIntervals());
+
+        private static Price CreateRelativePrice(CreateItemRequest request) =>
+            new Price(request.GetRelativePrice());
     }
 }
