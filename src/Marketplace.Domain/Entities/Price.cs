ee385b489bf41b5c84a5a1273ea2acefa000c42a
Davi Coutinho <davi.roox@hotmail.com>
2019-01-24 18:09:42 -0200

    entidade price com novas propriedades (#17)

--- src/Marketplace.Domain/Entities/Price.cs
+++ src/Marketplace.Domain/Entities/Price.cs
@@ -4,16 +4,30 @@ namespace Marketplace.Domain.Entities
 {
     public class Price : EntityBase
     {
-        public Price(bool @fixed)
+        public Price(bool @fixed, decimal value)
         {
             Fixed = @fixed;
+            Value = value;
         }
 
         public Price()
         {
             Fixed = false;
+            Value = 0;
+        }
+
+        public Price(bool @fixed)
+        {
+            Fixed = @fixed;
+        }
+
+        public Price(RelativePrice prices)
+        {
+            Prices = prices;
         }
 
         public bool Fixed { get; private set; }
+        public decimal Value { get; private set; }
+        public RelativePrice Prices { get; set; }
     }
 }
