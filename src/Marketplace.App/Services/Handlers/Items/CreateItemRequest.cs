16b90bd6f9517c3369113039239c1c07f728c23e
Davi Coutinho <davi.roox@hotmail.com>
2019-01-24 18:08:44 -0200

    Informações do body da requisição para preço de item (#17)

--- src/Marketplace.App/Services/Handlers/Items/CreateItemRequest.cs
+++ src/Marketplace.App/Services/Handlers/Items/CreateItemRequest.cs
@@ -1,9 +1,34 @@
 ﻿using MediatR;
+using System.Collections.Generic;
 
 namespace Marketplace.App.Services.Handlers.Items
 {
     public class CreateItemRequest : IRequest<CreateItemResponse>
     {
         public string Name { get; set; }
+
+        public bool Fixed { get; set; }
+
+        public class _FixedPrice
+        {
+            public decimal Value { get; set; }
+        }
+
+        public _FixedPrice FixedPrice { get; set; }
+
+        public class _RelativePrice
+        {
+            public class _Interval
+            {
+                public int Start { get; set; }
+                public int End { get; set; }
+                public decimal Value { get; set; }
+            }
+
+            public IEnumerable<_Interval> Table { get; set; }
+        }
+
+        public _RelativePrice RelativePrice { get; set; }
+
     }
 }
