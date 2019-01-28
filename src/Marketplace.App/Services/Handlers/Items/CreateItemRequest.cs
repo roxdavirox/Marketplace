7a6353e354385c6e9a9e53bdce48d59d12ac1321
Davi Coutinho <davi.roox@hotmail.com>
2019-01-28 10:19:07 -0200

    item request refatorado para receber array com os intervalos de preços (#19)

--- src/Marketplace.App/Services/Handlers/Items/CreateItemRequest.cs
+++ src/Marketplace.App/Services/Handlers/Items/CreateItemRequest.cs
@@ -7,28 +7,14 @@ namespace Marketplace.App.Services.Handlers.Items
     {
         public string Name { get; set; }
 
-        public bool Fixed { get; set; }
-
-        public class _FixedPrice
+        public class _Price
         {
+            public int Start { get; set; }
+            public int End { get; set; }
             public decimal Value { get; set; }
         }
 
-        public _FixedPrice FixedPrice { get; set; }
-
-        public class _RelativePrice
-        {
-            public class _Interval
-            {
-                public int Start { get; set; }
-                public int End { get; set; }
-                public decimal Value { get; set; }
-            }
-
-            public IEnumerable<_Interval> Table { get; set; }
-        }
-
-        public _RelativePrice RelativePrice { get; set; }
+        public IEnumerable<_Price> Prices { get; set; }
 
     }
 }
