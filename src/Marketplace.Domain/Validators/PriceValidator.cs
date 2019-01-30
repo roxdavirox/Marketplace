d3cc903a9603eda8fdb29f34f6371dabb9dd4b72
Davi Coutinho <davi.roox@hotmail.com>
2019-01-30 11:22:00 -0200

    validações para preço

--- src/Marketplace.Domain/Validators/PriceValidator.cs
+++ src/Marketplace.Domain/Validators/PriceValidator.cs
@@ -7,7 +7,17 @@ namespace Marketplace.Domain.Validators
     {
         public PriceValidator()
         {
-            RuleFor(_ => _.Start).NotEmpty().WithMessage("Start nao pode ser vazio");
+            RuleFor(_ => _.Start)
+                .NotEmpty().WithMessage("Start nao pode ser vazio")
+                .GreaterThan(0).WithMessage("Start não pode ser menor que 1");
+
+            RuleFor(_ => _.End)
+                .GreaterThan(0).WithMessage("End não pode ser menor que 1")
+                .NotEmpty().WithMessage("End não pode ser vazio");
+
+            RuleFor(_ => _.Value)
+                .GreaterThan(0).WithMessage("Value não pode ser menor que 1")
+                .NotEmpty().WithMessage("Value não pode ser vazio");
         }
     }
 }
