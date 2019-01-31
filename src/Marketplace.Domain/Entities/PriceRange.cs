3a57b5e44d8f8e4960bd1ad6df8fed8a9bfff834
roxdavirox <roxdavirox@gmail.com>
2019-01-31 18:14:20 -0200

    validator dentro da entidade PriceRange (#23)

--- src/Marketplace.Domain/Entities/PriceRange.cs
+++ src/Marketplace.Domain/Entities/PriceRange.cs
@@ -1,11 +1,15 @@
 ﻿using Marketplace.Domain.Entities.Base;
+using Marketplace.Domain.Validators;
 using System.Collections.Generic;
 
 namespace Marketplace.Domain.Entities
 {
     public class PriceRange : EntityBase
     {
-        public IEnumerable<Price> Prices { get; private set; }
-        public virtual IEnumerable<Item> Items { get; private set; }
+        public virtual ICollection<Item> Items { get; private set; }
+        public ICollection<Price> Prices { get; private set; }
+
+        public PriceRange() { Validate(this, new PriceRangeValidator()); }
+
     }
 }
