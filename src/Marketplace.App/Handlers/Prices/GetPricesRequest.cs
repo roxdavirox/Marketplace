ebde2fee4145ac97b86597befb9632fe77a8edb8
roxdavirox <roxdavirox@gmail.com>
2019-04-04 15:27:40 -0300

    recebendo id do price range no request

--- src/Marketplace.App/Handlers/Prices/GetPricesRequest.cs
+++ src/Marketplace.App/Handlers/Prices/GetPricesRequest.cs
@@ -5,6 +5,11 @@ namespace Marketplace.App.Handlers.Prices
 {
     public class GetPricesRequest : IRequest<GetPricesResponse>
     {
+        public GetPricesRequest(Guid idPriceRange)
+        {
+            IdPriceRange = idPriceRange;
+        }
+
         public Guid IdPriceRange { get; set; }
     }
 }
