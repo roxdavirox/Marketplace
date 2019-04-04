fd74e7f2244eacede8e0bac376179163fd3ac83a
roxdavirox <roxdavirox@gmail.com>
2019-04-04 09:02:12 -0300

    name adicionado a entidade priceRange

--- src/Marketplace.Domain/Entities/PriceRange.cs
+++ src/Marketplace.Domain/Entities/PriceRange.cs
@@ -6,10 +6,16 @@ namespace Marketplace.Domain.Entities
 {
     public class PriceRange : EntityBase
     {
+        public string Name { get; private set; }
+
         public virtual ICollection<Item> Items { get; private set; }
         public ICollection<Price> Prices { get; private set; }
+        
+        public PriceRange(string name)
+        {
+            Name = name;
 
-        public PriceRange() { Validate(this, new PriceRangeValidator()); }
-
+            Validate(this, new PriceRangeValidator());
+        }
     }
 }
