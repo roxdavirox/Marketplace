00507633e05d587c7f4bb8d919e37c812781b8c1
roxdavirox <roxdavirox@gmail.com>
2019-04-04 15:13:16 -0300

    response dos prices implementado

---
+++ src/Marketplace.App/Handlers/Prices/GetPricesResponse.cs
@@ -0,0 +1,34 @@
+﻿using Marketplace.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Marketplace.App.Handlers.Prices
+{
+    public class _Price
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public decimal Value { get; set; }
+    }
+
+    public class GetPricesResponse
+    {
+        public List<_Price> Prices { get; set; }
+
+        public GetPricesResponse(IEnumerable<Price> prices)
+        {
+            if (prices == null) return;
+
+            Prices = new List<_Price>();
+
+            foreach (var price in prices)
+            {
+                Prices.Add(new _Price
+                {
+                    Start = price.Start,
+                    End = price.End,
+                    Value = price.Value
+                });
+            }
+        }
+    }
+}
