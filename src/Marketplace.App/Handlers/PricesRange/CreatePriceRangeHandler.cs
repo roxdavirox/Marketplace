01f4696bbc31a79161a16b7794252806fbf8a4be
roxdavirox <roxdavirox@gmail.com>
2019-04-04 11:20:07 -0300

    prices removido do handler do price range quando criado

--- src/Marketplace.App/Handlers/PricesRange/CreatePriceRangeHandler.cs
+++ src/Marketplace.App/Handlers/PricesRange/CreatePriceRangeHandler.cs
@@ -1,10 +1,7 @@
 ﻿using Marketplace.App.Notifications;
 using Marketplace.Domain.Entities;
 using Marketplace.Domain.Interfaces.Repositories;
-using Marketplace.Shared.Extensions;
 using MediatR;
-using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,16 +11,13 @@ namespace Marketplace.App.Handlers.PricesRange
         IRequestHandler<CreatePriceRangeRequest, CreatePriceRangeResponse>
     {
         private readonly NotificationContext _notificationContext;
-        private readonly IPriceRepository _priceRepository;
         private readonly IPriceRangeRepository _priceRangeRepository;
 
         public CreatePriceRangeHandler
             (NotificationContext notificationContext,
-            IPriceRepository priceRepository,
             IPriceRangeRepository priceRangeRepository)
         {
             _notificationContext = notificationContext;
-            _priceRepository = priceRepository;
             _priceRangeRepository = priceRangeRepository;
         }
 
@@ -36,29 +30,13 @@ namespace Marketplace.App.Handlers.PricesRange
                 return null;
             }
 
-            var prices = request.Prices
-                .Select(p => new Price(p.Start, p.End, p.Value))
-                .ToList();
-
-            var invalidPrice = prices.Any(p => p.Invalid);
-
-            if (invalidPrice)
-            {
-                prices.Where(_ => _.Invalid)
-                    .ForEach(p => _notificationContext.AddNotifications(p.ValidationResult));
-                return null;
-            }
-
             var priceRange = new PriceRange(request.Name);
-            priceRange.HasMany(prices);
 
             if (priceRange.Invalid)
             {
                 _notificationContext.AddNotifications(priceRange.ValidationResult);
                 return null;
             }
-
-            await _priceRepository.CreateRangeAsync(prices);
 
             await _priceRangeRepository.CreateAsync(priceRange);
 
