ae622f71998c5528e3bb11eb691a59479e32a8d4
roxdavirox <roxdavirox@gmail.com>
2019-04-04 14:23:06 -0300

    interface com get all async dos prices range

--- src/Marketplace.Domain/Interfaces/Repositories/IPriceRangeRepository.cs
+++ src/Marketplace.Domain/Interfaces/Repositories/IPriceRangeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Marketplace.Domain.Entities;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Marketplace.Domain.Interfaces.Repositories
@@ -8,5 +9,6 @@ namespace Marketplace.Domain.Interfaces.Repositories
     {
         Task<PriceRange> CreateAsync(PriceRange priceRange);
         Task<PriceRange> GetByIdAsync(Guid idPriceRange);
+        Task<IEnumerable<PriceRange>> GetAllAsync();
     }
 }
