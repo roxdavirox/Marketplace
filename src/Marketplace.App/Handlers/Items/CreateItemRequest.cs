a09b8fe0b88acb209d649ffae6fcba1ca39ded3f
roxdavirox <roxdavirox@gmail.com>
2019-04-04 10:34:07 -0300

    array de prices removido do request da criação de um item - é necessario apenas o nome - associar o item depois com o priceRange

--- src/Marketplace.App/Handlers/Items/CreateItemRequest.cs
+++ src/Marketplace.App/Handlers/Items/CreateItemRequest.cs
@@ -1,20 +1,9 @@
 ﻿using MediatR;
-using System.Collections.Generic;
 
 namespace Marketplace.App.Handlers.Items
 {
     public class CreateItemRequest : IRequest<CreateItemResponse>
     {
         public string Name { get; set; }
-
-        public class CreateItemRequest_Price
-        {
-            public int Start { get; set; }
-            public int End { get; set; }
-            public decimal Value { get; set; }
-        }
-
-        public IEnumerable<CreateItemRequest_Price> Prices { get; set; }
-
     }
 }
