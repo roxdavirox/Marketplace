db0adbedfaed5940063d601dc04382c3c788783b
roxdavirox <roxdavirox@gmail.com>
2019-04-04 14:41:17 -0300

    usings desnecessarios removidos

--- src/Marketplace.App/Handlers/PricesRange/GetAllPricesRangesHandler.cs
+++ src/Marketplace.App/Handlers/PricesRange/GetAllPricesRangesHandler.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Marketplace.App.Notifications;
+using Marketplace.Domain.Interfaces.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Marketplace.App.Handlers.PricesRange
 {
-    class GetAllPricesRangesHandler
+    public class GetAllPricesRangesHandler
+        : IRequestHandler<GetAllPricesRangeRequest, GetAllPricesRangeResponse>
     {
+        private readonly NotificationContext _notificationContext;
+        private readonly IPriceRangeRepository _priceRangeRepository;
+
+        public GetAllPricesRangesHandler(
+            NotificationContext notificationContext, IPriceRangeRepository priceRangeRepository)
+        {
+            _notificationContext = notificationContext;
+            _priceRangeRepository = priceRangeRepository;
+        }
+
+        public async Task<GetAllPricesRangeResponse> Handle(
+            GetAllPricesRangeRequest request, CancellationToken cancellationToken)
+        {
+            if(request == null)
+            {
+                _notificationContext.AddNotification("Request", "o request não pode ser nulo");
+                return null;
+            }
+
+            var pricesRange = await _priceRangeRepository.GetAllAsync();
+
+            return new GetAllPricesRangeResponse(pricesRange);
+        }
     }
 }
