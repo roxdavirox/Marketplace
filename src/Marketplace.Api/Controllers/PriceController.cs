294bdc26b83691f40e7176de6ba86f4926b65549
roxdavirox <roxdavirox@gmail.com>
2019-04-04 12:00:26 -0300

    endpoint com POST para criar um price associado a um price range - recebendo id na url

---
+++ src/Marketplace.Api/Controllers/PriceController.cs
@@ -0,0 +1,24 @@
+﻿using Marketplace.App.Handlers.Prices;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Marketplace.Api.Controllers
+{
+    [ApiController]
+    public class PriceController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PriceController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("api/Price/{idPriceRange:Guid}"), AllowAnonymous]
+        public async Task<CreatePriceResponse> Post(CreatePriceRequest request, Guid idPriceRange) =>
+            await _mediator.Send(new CreatePriceRequest(request, idPriceRange));
+    }
+}
