b532b40846e508cb189023dca62628f27e1069f3
roxdavirox <roxdavirox@gmail.com>
2019-04-04 10:23:06 -0300

    crate item handler modificado para criar apenas o item com seu nome

--- src/Marketplace.App/Handlers/Items/CreateItemHandler.cs
+++ src/Marketplace.App/Handlers/Items/CreateItemHandler.cs
@@ -1,9 +1,7 @@
 ﻿using Marketplace.App.Notifications;
 using Marketplace.Domain.Entities;
-using Marketplace.Shared.Extensions;
 using Marketplace.Domain.Interfaces.Repositories;
 using MediatR;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,35 +10,20 @@ namespace Marketplace.App.Handlers.Items
     public class CreateItemHandler : IRequestHandler<CreateItemRequest, CreateItemResponse>
     {
         private readonly IItemRepository _itemRepository;
-        private readonly IPriceRepository _priceRepository;
-        private readonly IPriceRangeRepository _priceRangeRepository;
         private readonly NotificationContext _notificationContext;
 
         public CreateItemHandler(
-            IItemRepository itemRepository, 
-            IPriceRepository priceRepository, 
-            IPriceRangeRepository priceRangeRepository, 
+            IItemRepository itemRepository,
             NotificationContext notificationContext)
         {
             _itemRepository = itemRepository;
-            _priceRepository = priceRepository;
-            _priceRangeRepository = priceRangeRepository;
             _notificationContext = notificationContext;
         }
 
         public async Task<CreateItemResponse> Handle(
             CreateItemRequest request, CancellationToken cancellationToken)
-        {
-            var priceRange = new PriceRange();
-
-            if (priceRange.Invalid)
-            {
-                _notificationContext.AddNotifications(priceRange.ValidationResult);
-                return null;
-            }
-
+        {       
             var item = new Item(request.Name);
-            item.HasOne(priceRange);
 
             if (item.Invalid)
             {
@@ -48,28 +31,8 @@ namespace Marketplace.App.Handlers.Items
                 return null;
             }
 
-            var prices = request.Prices
-                .Select(p => new Price(p.Start, p.End, p.Value))
-                .ToList();
-
-            prices.ForEach(p => p.HasOne(priceRange));
-
-            var invalidPrices = prices.Where(p => p.Invalid);
-
-            var invalidPrice = invalidPrices.Any();
-
-            if(invalidPrice)
-            {
-                invalidPrices.ForEach(p => _notificationContext.AddNotifications(p.ValidationResult));
-                return null;
-            }
-
-            await _priceRangeRepository.CreateAsync(priceRange);
-
             await _itemRepository.CreateAsync(item);
-
-            await _priceRepository.CreateRangeAsync(prices);
-
+            
             return (CreateItemResponse)item;
         }
     }
