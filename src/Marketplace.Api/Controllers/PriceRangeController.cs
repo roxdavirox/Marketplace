759a8ddd128c37640e0e06a424b397baef9efa79
roxdavirox <roxdavirox@gmail.com>
2019-04-04 10:09:18 -0300

    controller do price range com endpoint para adicionar os preços

---
+++ src/Marketplace.Api/Controllers/PriceRangeController.cs
@@ -0,0 +1,23 @@
+﻿using Marketplace.App.Handlers.PricesRange;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Marketplace.Api.Controllers
+{
+    [ApiController]
+    public class PriceRangeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PriceRangeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("api/PriceRange"), AllowAnonymous]
+        public async Task<CreatePriceRangeResponse> Post(CreatePriceRangeRequest request) =>
+            await _mediator.Send(request);
+    }
+}
