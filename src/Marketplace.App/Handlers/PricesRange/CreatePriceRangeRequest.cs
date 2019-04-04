952c76333c46d46826271584549718a636db4afd
roxdavirox <roxdavirox@gmail.com>
2019-04-04 11:17:25 -0300

    criando price range apenas com nome - associar o intervalo do preço no endpoint do price

--- src/Marketplace.App/Handlers/PricesRange/CreatePriceRangeRequest.cs
+++ src/Marketplace.App/Handlers/PricesRange/CreatePriceRangeRequest.cs
@@ -6,14 +6,5 @@ namespace Marketplace.App.Handlers.PricesRange
     public class CreatePriceRangeRequest : IRequest<CreatePriceRangeResponse>
     {
         public string Name { get; set; }
-
-        public class _PriceRange
-        {
-            public int Start { get; set; }
-            public int End { get; set; }
-            public decimal Value { get; set; }
-        }
-
-        public IEnumerable<_PriceRange> Prices { get; set; }
     }
 }
