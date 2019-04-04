49275794aa4ab42b8c7e39dc5b9fbecb05ab18fc
roxdavirox <roxdavirox@gmail.com>
2019-04-04 14:40:23 -0300

    put handler para adicionar uma referencia a um priceRange existente

---
+++ src/Marketplace.App/Handlers/Items/PutItemRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Marketplace.App.Handlers.Items
+{
+    public class PutItemRequest : IRequest<PutItemResponse>
+    {
+        public Guid IdPriceRange { get; set; }
+        public Guid IdItem { get; set; }
+    }
+}
---
+++ src/Marketplace.App/Handlers/Items/PutItemResponse.cs
@@ -0,0 +1,23 @@
+﻿using Marketplace.Domain.Entities;
+using System;
+
+namespace Marketplace.App.Handlers.Items
+{
+    public class PutItemResponse
+    {
+        public Guid IdPriceRange { get; set; }
+        public string PriceRangeName { get; set; }
+
+        public Guid IdItem { get; set; }
+        public string ItemName { get; set; }
+
+        public PutItemResponse(PriceRange priceRange, Item item)
+        {
+            IdPriceRange = priceRange.Id;
+            PriceRangeName = priceRange.Name;
+
+            IdItem = item.Id;
+            ItemName = item.Name;
+        }
+    }
+}
