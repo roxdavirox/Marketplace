2b6cec7f7e022c72c1b18cfa57b1bfcdc895597a
roxdavirox <roxdavirox@gmail.com>
2019-04-05 13:05:58 -0300

    actions do controller do item documentado com xml para exibir no swagger

--- src/Marketplace.Api/Controllers/ItemController.cs
+++ src/Marketplace.Api/Controllers/ItemController.cs
@@ -17,10 +17,21 @@ namespace Marketplace.Api.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Criar um Item no sistema
+        /// </summary>
+        /// <param name="request">Request com Nome do Item</param>
+        /// <returns>Retorna o Nome e o Id do Item</returns>
         [HttpPost("api/Items"), AllowAnonymous]
         public async Task<CreateItemResponse> Post(CreateItemRequest request) =>
             await _mediator.Send(request);
 
+        /// <summary>
+        /// Cria um Item dentro de uma Opção existente
+        /// </summary>
+        /// <param name="request"> Request com nome do Item</param>
+        /// <param name="idOption">Id da Opção que receberá um Item</param>
+        /// <returns>Retorna o Id e Nome do Item criado</returns>
         [HttpPost("api/Options/{idOption}/Items"), AllowAnonymous]
         public async Task<CreateItemResponse> Post(CreateItemOptionRequest request, Guid idOption)
         {
@@ -29,15 +40,32 @@ namespace Marketplace.Api.Controllers
             return response;
         }
 
+        /// <summary>
+        /// Obtem todos os Itens associados com uma Opção
+        /// </summary>
+        /// <param name="idOption">Id da opção que deseja obter os itens</param>
+        /// <returns>Retorna todos os Itens associados a uma Opção</returns>
         [HttpGet("api/Items/{idOption}"), AllowAnonymous]
         public async Task<GetItemsByOptionIdResponse> GetByOptionId(Guid idOption) =>
             await _mediator.Send( new GetItemsByOptionIdRequest(idOption));
 
+        /// <summary>
+        /// Deleta um ou mais Itens a partir de seus Ids
+        /// </summary>
+        /// <param name="request">Request com um array de Ids dos Itens que serão deletados</param>
+        /// <returns>Retorna a quantidade de Itens que foram deletados</returns>
         [HttpDelete("api/Items"), AllowAnonymous]
         public async Task<DeleteMultiplesItemsResponse> DeleteMultiples(
                 DeleteMultiplesItemsRequest request
             ) => await _mediator.Send(request);
 
+        /// <summary>
+        /// Adiciona uma Tabela de Preço(PriceRange) ao item, mesmo que ele já tenha sido associado com
+        /// outra Tabela de Preço
+        /// </summary>
+        /// <param name="request">Request com o Id do Item e o Id da Tabela de Preço 
+        /// que o item será associado</param>
+        /// <returns>Retorna os Nomes e Ids do Item e da Tabela de Preço</returns>
         [HttpPut("api/Items"), AllowAnonymous]
         public async Task<PutItemResponse> Put(PutItemRequest request) =>
             await _mediator.Send(request);
