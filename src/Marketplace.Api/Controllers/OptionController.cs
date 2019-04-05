ead2193efabbd4f5809cf11c02c24e26e8efaaaf
roxdavirox <roxdavirox@gmail.com>
2019-04-05 13:18:57 -0300

    OptionsController com rotas documentadas no swagger

--- src/Marketplace.Api/Controllers/OptionController.cs
+++ src/Marketplace.Api/Controllers/OptionController.cs
@@ -17,6 +17,12 @@ namespace Marketplace.Api.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Cria uma Opção associando com um Produto existente
+        /// </summary>
+        /// <param name="request">Request com o nome da Opção quer será criada</param>
+        /// <param name="idProduct">Id do produto que será associado com a nova Opção</param>
+        /// <returns>Retorna o Nome e Id da Opção criada</returns>
         [HttpPost("api/Products/{idProduct:Guid}/Options"), AllowAnonymous]
         public async Task<CreateOptionResponse> Post(CreateOptionProductRequest request, Guid idProduct)
         {
@@ -24,14 +30,30 @@ namespace Marketplace.Api.Controllers
             return await _mediator.Send(command);
         }
 
+        /// <summary>
+        /// Cria uma nova Opção no sistema
+        /// </summary>
+        /// <param name="request">Request com o nome da nova Opção</param>
+        /// <returns>Retorna o Id e Nome da Opção criada</returns>
         [HttpPost("api/Options"), AllowAnonymous]
         public async Task<CreateOptionResponse> Post(CreateOptionRequest request) =>
             await _mediator.Send(request);
 
+        /// <summary>
+        /// Associa um Item existente a uma Opção existente
+        /// </summary>
+        /// <param name="request">Request com os Ids do Item e da Opção</param>
+        /// <returns>Retorna os Ids e Nomes da Opção e do Item associados</returns>
         [HttpPut("api/Options/Items"), AllowAnonymous]
         public async Task<PutOptionItemResponse> Put(PutOptionItemRequest request) =>
             await _mediator.Send(request);
 
+        /// <summary>
+        /// Cria uma nova Opção e associa com um Item existente no sistema
+        /// </summary>
+        /// <param name="request">Request com o nome da Opção que será criada</param>
+        /// <param name="idItem">Id do Item que a nova Opção será associada</param>
+        /// <returns>Retorna os Ids e Nomes da Opção e do Item associados</returns>
         [HttpPost("api/Items/{idItem:Guid}/Options"), AllowAnonymous]
         public async Task<CreateOptionItemResponse> Post(CreateOptionItemRequest request, Guid idItem)
         {
@@ -42,10 +64,19 @@ namespace Marketplace.Api.Controllers
             return response;
         }
 
+        /// <summary>
+        /// Obtem todas as Opções existentes no sistema
+        /// </summary>
+        /// <returns>Retorna uma lista com Id e Nome de cada Opção</returns>
         [HttpGet("api/Options"), AllowAnonymous]
         public async Task<GetAllOptionsResponse> GetAll() =>
             await _mediator.Send(new GetAllOptionsRequest());
 
+        /// <summary>
+        /// Deleta uma ou mais Opções a partir de seus Ids
+        /// </summary>
+        /// <param name="request">Request com uma lista de Ids de Opções que serão deletadas</param>
+        /// <returns>Retorna a quantidade de Opções deletadas</returns>
         [HttpDelete("api/Options"), AllowAnonymous]
         public async Task<DeleteOptionsResponse> DeleteMultiples(
                 DeleteOptionsRequest request
