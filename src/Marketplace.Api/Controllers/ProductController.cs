a0c67b8b27e5855ec0a0ffc8b3f73d757c992e67
roxdavirox <roxdavirox@gmail.com>
2019-04-05 13:23:41 -0300

    ProductController com rotas documentadas no swagger

--- src/Marketplace.Api/Controllers/ProductController.cs
+++ src/Marketplace.Api/Controllers/ProductController.cs
@@ -16,10 +16,20 @@ namespace Marketplace.Api.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Cria um novo Produto no sistema
+        /// </summary>
+        /// <param name="request">Request com o Nome do Produto</param>
+        /// <returns>Retorna o Nome e Id do Produto criado</returns>
         [HttpPost("api/Products"), AllowAnonymous]
         public async Task<CreateProductResponse> Post(CreateProductRequest request) =>
             await _mediator.Send(request);
 
+        /// <summary>
+        /// Associa uma opção existente a um Produto existente
+        /// </summary>
+        /// <param name="request">Request com Id do Produto e Opção existente</param>
+        /// <returns>Retorna os Ids e Nomes do Produto e Opção associados</returns>
         [HttpPut("api/Products/Options"), AllowAnonymous]
         public async Task<PutProductOptionResponse> Put(PutProductOptionRequest request) =>
             await _mediator.Send(request);
