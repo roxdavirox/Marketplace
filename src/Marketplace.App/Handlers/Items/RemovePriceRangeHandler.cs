4859dbe591230000bc3c783f267b2c5dd1fe630a
roxdavirox <roxdavirox@gmail.com>
2019-04-12 11:47:44 -0300

    code style

--- src/Marketplace.App/Handlers/Items/RemovePriceRangeHandler.cs
+++ src/Marketplace.App/Handlers/Items/RemovePriceRangeHandler.cs
@@ -4,37 +4,41 @@ using Marketplace.App.Notifications;
 using Marketplace.Domain.Interfaces.Repositories;
 using MediatR;
 
-namespace Marketplace.App.Handlers.Items {
-  public class RemovePriceRangeHadler : IRequestHandler<RemovePriceRangeRequest, RemovePriceRangeResponse> {
-
-    private readonly NotificationContext _notificationContext;
-    private readonly IItemRepository _itemRepository;
-
-    public RemovePriceRangeHadler(
-      NotificationContext notificationContext, IItemRepository itemRepository)
+namespace Marketplace.App.Handlers.Items
+{
+    public class RemovePriceRangeHadler : IRequestHandler<RemovePriceRangeRequest, RemovePriceRangeResponse>
     {
-      _notificationContext = notificationContext;
-      _itemRepository = itemRepository;
+
+        private readonly NotificationContext _notificationContext;
+        private readonly IItemRepository _itemRepository;
+
+        public RemovePriceRangeHadler(
+          NotificationContext notificationContext, IItemRepository itemRepository)
+        {
+            _notificationContext = notificationContext;
+            _itemRepository = itemRepository;
+        }
+
+        public async Task<RemovePriceRangeResponse> Handle(
+          RemovePriceRangeRequest request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                _notificationContext.AddNotification("Request", "request nulo");
+                return null;
+            }
+
+            var item = await _itemRepository.GetByIdAsync(request.IdItem);
+
+            if (item == null)
+            {
+                _notificationContext.AddNotification("Item", "Não encontrado");
+                return null;
+            }
+
+            item.RemovePriceRange();
+
+            return new RemovePriceRangeResponse(item);
+        }
     }
-
-    public async Task<RemovePriceRangeResponse> Handle(
-      RemovePriceRangeRequest request, CancellationToken cancellationToken)
-    {
-      if (request == null) {
-        _notificationContext.AddNotification("Request", "request nulo");
-        return null;
-      }
-
-      var item = await _itemRepository.GetByIdAsync(request.IdItem);
-
-      if(item == null) {
-        _notificationContext.AddNotification("Item","Não encontrado");
-        return null;
-      }
-
-      item.RemovePriceRange();
-
-      return new RemovePriceRangeResponse(item);
-    }
-  }
 }
