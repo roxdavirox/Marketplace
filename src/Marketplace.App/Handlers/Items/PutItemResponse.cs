b1c2f4ae7702b3f225d8e8f22d5e95c99fed7fe3
roxdavirox <roxdavirox@gmail.com>
2019-04-12 09:37:25 -0300

    retornando o item no response quando o item é atualizado

--- src/Marketplace.App/Handlers/Items/PutItemResponse.cs
+++ src/Marketplace.App/Handlers/Items/PutItemResponse.cs
@@ -6,18 +6,15 @@ namespace Marketplace.App.Handlers.Items
     public class PutItemResponse
     {
         public Guid IdPriceRange { get; set; }
-        public string PriceRangeName { get; set; }
-
         public Guid IdItem { get; set; }
-        public string ItemName { get; set; }
+        public string Name { get; set; }
 
         public PutItemResponse(PriceRange priceRange, Item item)
         {
             IdPriceRange = priceRange.Id;
-            PriceRangeName = priceRange.Name;
 
             IdItem = item.Id;
-            ItemName = item.Name;
+            Name = item.Name;
         }
     }
 }
